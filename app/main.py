43043c26dbafa119107c1f5a0d32fa6fb40e22e1
mykolashashliyk-coder <mykolashashliyk@gmail.com>
2026-01-10 16:53:33 +0000

    Implement linter formatter functions

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error.get("line_number", error.get("line")),
+        "column": error.get("column_number", error.get("colunm")),
+        "message": error.get("text", error.get("message")),
+        "name": error.get("code", error.get("name")),
+        "source": error.get("source","flake8"),
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "path": file_path,
+        "errors": [format_linter_error(e) for e in errors],
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
