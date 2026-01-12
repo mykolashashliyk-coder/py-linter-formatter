8d62c2964a01c58d15b2bf59beaf07a8d00f1495
mykolashashliyk-coder <mykolashashliyk@gmail.com>
2026-01-12 11:34:48 +0000

    Fix flake8 hanging indent issues

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def format_linter_error(error: dict) -> dict:
     return {
-         "line": error.get("line_number"),
+        "line": error.get("line_number"),
         "column": error.get("column_number"),
         "message": error.get("text"),
         "name": error.get("code"),
@@ -11,7 +11,10 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "path": file_path,
-        "errors": [format_linter_error(error) for error in errors],
+        "errors": [
+            format_linter_error(error)
+            for error in errors
+        ],
         "status": "failed" if errors else "passed"
     }
 
